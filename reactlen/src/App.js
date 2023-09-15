d9ea73485f4d2d4457500ee433efe29bfd4d3b31
aazzizzz1 <azizvbi33@gmail.com>
2023-09-15 15:08:19 +0700

    coba reactlen

---
+++ reactlen/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import FlowSide from './Components/Sidebar/FlowSide';
+import Table from './Components/Table/Table';
+
+function App() {
+  return (
+    <>
+    <FlowSide/>
+    <Table/>
+    </>
+  );
+}
+
+export default App;
