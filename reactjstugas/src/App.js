0100cb5cfc567ee3030a3804b4a152605e5b38b4
aazzizzz1 <azizvbi33@gmail.com>
2023-09-15 13:07:28 +0700

    add

--- reactjstugas/src/App.js
+++ reactjstugas/src/App.js
@@ -4,14 +4,26 @@ import Tugas8 from './Components/ComponentsProps/ComponentProps';
 import './Style/style.css';
 import Button from './Components/ComponentsProps/Button';
 import StateHooks from './Components/StateHooks/Materi';
+import Tailwind from './Components/Tailwind/Tailwind';
+import Pagination from './Components/Tailwind/Pagination';
+import Sidebar from './Components/Tailwind/Sidebar';
+import Side from './Components/Tailwind/Side';
+import Pagin from './Components/Tailwind/Pagin';
+import SideFlow from './Components/Tailwind/SideFlow';
 
 function App() {
   return (
     <>
       {/* <Tugas7/> */}
-      <Tugas8 name="Aziz" adress="Banyuwangi" email="azizvbi33@gmail.com" user="azizvbi"/>
-      <Button/>
-      <StateHooks/>
+      {/* <Sidebar/> */}
+      {/* <Tailwind/> */}
+      <Side/>
+      {/* <SideFlow/> */}
+      {/* <Tugas8 name="Aziz" adress="Banyuwangi" email="azizvbi33@gmail.com" user="azizvbi"/> */}
+      {/* <Button/> */}
+      {/* <StateHooks/> */}
+      {/* <Pagin/> */}
+      {/* <Pagination/> */}
     </>
   );
 }
